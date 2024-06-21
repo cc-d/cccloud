50830ecace54a24e558da3081ec1794cc151670c
Cary Carter <ccarterdev@gmail.com>
2024-06-21 04:02:31 -0700

    fixed docker error

--- app/config.py
+++ app/config.py
@@ -3,8 +3,9 @@ import os.path as op
 from pyshared import typed_evar as evar
 
 # Configuration file for future extensibility
-BASE_DIR = evar('CCC_UPLOAD_DIR', op.dirname(op.dirname(op.abspath(__file__))))
-UPLOAD_DIR = op.join(BASE_DIR, 'uploads')
+BASE_DIR = evar('CCC_DIR', op.dirname(op.dirname(op.abspath(__file__))))
+
+UPLOAD_DIR = evar('CCC_UPLOAD_DIR', op.join(BASE_DIR, 'uploads'))
 
 if not op.exists(UPLOAD_DIR):
     os.makedirs(UPLOAD_DIR)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
+import sys
+import os.path as op
+
+sys.path.insert(0, op.dirname(op.dirname(op.abspath(__file__))))
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes import app
+
+from app.routes import app
 
 origins = ["http://localhost:3000"]  # Add your frontend URL here
 
